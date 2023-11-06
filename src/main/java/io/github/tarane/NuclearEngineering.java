bb96cca54055d457b871d4d8e2b3c0410a142791
TaraDaReal <tarantulagamingdareal@gmail.com>
2023-11-06 12:12:51 +0200

    Create

---
+++ src/main/java/io/github/tarane/NuclearEngineering.java
@@ -0,0 +1,13 @@
+package io.github.tarane;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NuclearEngineering implements ModInitializer {
+
+    public static final String MODID = "tarane";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/io/github/tarane/client/NuclearEngineeringClient.java
@@ -0,0 +1,13 @@
+package io.github.tarane.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NuclearEngineeringClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
